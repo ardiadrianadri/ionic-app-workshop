60fca1414e9c91b37e2b2c72fab0d626f1f42506
ardiadrianadri <ardiadrianadri@gmail.com>
2018-10-11 07:40:05 +0200

    Add logout action

--- src/pages/nav-menu/nav-menu.ts
+++ src/pages/nav-menu/nav-menu.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 import { ListCatsPage } from '../list-cats/list-cats';
+import { UserApi } from '../../core';
+import { Response } from '../../common';
 @Component({
   selector: 'nav-menu',
   templateUrl: 'nav-menu.html'
@@ -8,5 +11,16 @@ import { ListCatsPage } from '../list-cats/list-cats';
 export class NavMenuPage {
 
   public rootPage = ListCatsPage;
-  constructor() { }
+  
+  constructor(
+    private _userService: UserApi,
+    private _navCtrl: NavController
+  ) { }
+
+  public logoutNavigation() {
+    this._userService.closeSession().subscribe(
+      () => { this._navCtrl.pop(); },
+      (error: Response) => { throw new Error(error.message); }
+    );
+  }
 }
